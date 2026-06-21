2c57535e6e88f5d1555920bb7281875781c07299
cubewin <tanthang071208@gmail.com>
2026-06-22 11:25:46 +1200

    Create main + Student class

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿class Test
+{
+    public static void Main(String[] strings)
+    {
+        
+    }
+}
+
+class Student
+{
+    private double _gpa;
+    public string Name {get; set;}
+    public int Age {get; set;}
+
+    public Student(string name, int age, double gpa)
+    {
+        Name = name;
+        Age = age;
+        GPA = gpa;
+    }
+
+    public double GPA
+    {
+        get => _gpa;
+
+        set
+        {
+            if(value < 0 || value > 4) throw new ArgumentException("Invalid GPA");
+            _gpa = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}, GPA: {_gpa}";
+    }
+}
--- Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net10.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f32d7c2d57de195ddfd9660c4ef458e0d3340bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ac7e76bc6769ae49ba79b65f18c8ad561bbc69")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
