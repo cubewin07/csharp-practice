437ec918133e1f57a415dea7d920eedd80fd4bf6
cubewin <tanthang071208@gmail.com>
2026-07-14 20:09:57 +1200

    Exercise 1

---
+++ FinalDay/Program.cs
@@ -0,0 +1,21 @@
+﻿class FinalDay
+{
+    public static void Main(string[] args)
+    {
+        List<int> nums = [2,5,4,6];
+
+        Console.WriteLine(Sum(nums));
+    }
+
+    public static int Sum(List<int> nums)
+    {
+        int sum = 0;
+
+        foreach(int num in nums)
+        {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
